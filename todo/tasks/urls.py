6aef7cd544cb81db16dd9b39db35a672d64ecf9b
jadelcruz1 <jardelcruz6@gmail.com>
2021-12-02 15:03:07 -0300

    inserir dados do banco de dados

--- todo/tasks/urls.py
+++ todo/tasks/urls.py
@@ -5,6 +5,7 @@ from .import views
 urlpatterns = [
     path('helloworld/', views.helloworld),
     path('', views.tasksList, name='task-list'), # as aspas vazias sao para chamar a pagina home do projeto.
+    path('task/<int:id>', views.taskView, name="task-view"),
     path('yourname/<str:name>', views.yourName, name='your-name'), # posso passar um paramentro para ser impresso na tamplate.
     
 ]
--- todo/tasks/views.py
+++ todo/tasks/views.py
@@ -1,12 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse, request
 
+from .models import Task
+
+
+    
+    
 def helloworld(request):
     return HttpResponse('teste aplicação')
 
 def tasksList(request):
-    return render(request, 'tasks/list.html') #para criar uma view sempre terá de ter o request e o return.
+    tasks = Task.objects.all()
+    return render(request, 'tasks/list.html', {'tasks':tasks}
+) #para criar uma view sempre terá de ter o request e o return.
 
 def yourName(request, name):
     return render (request, 'tasks/yourname.html', {'name': name})
 
+def taskView(request, id):
+    task = get_object_or_404(Task, pk=id)
+    return render(request, 'tasks/task.html', {'task': task})
